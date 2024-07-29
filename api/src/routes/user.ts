0260a0dd6b00299d7176c53dbf2c6cfefd38512e
JTrenerry <105094182+JTrenerry@users.noreply.github.com>
2024-07-29 19:52:14 +1000

    added .env to gitignore

--- api/src/routes/user.ts
+++ api/src/routes/user.ts
@@ -1,1 +1,20 @@
-import {
+import { Request, Response } from 'express';
+import { UserRouteParams } from '../types';
+
+import { getConnection } from '../db';
+import { User as UserDb } from '../db/User';
+
+export async function postUser(req: Request<UserRouteParams, any, any>, res: Response) {
+    const { userId } = req.params;
+   
+   if (!userId) {
+       res.status(400).json('Missing userId');
+       return;
+   }
+
+   const userRepository = getConnection.getRepository(UserDb);
+
+   const newUser = new UserDb();
+
+   // Add picture logic
+   // 
